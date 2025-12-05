259d559dada1348b9f5a925142da98a447fcbd7a
Harry <68599167+harrykeeran12@users.noreply.github.com>
2025-12-05 00:37:38 +0000

    feat: solved day-4 part 1, need to solve part 2

---
+++ day-4/main.go
@@ -0,0 +1,106 @@
+package day4
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+// Read file
+// Convert a file into a two dimensional array of characters
+func arrayInit(filename string) [][]string{
+	f, err := os.ReadFile(filename)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	file := string(f)
+
+	fileArray := strings.Split(file, "\n")
+
+
+	var arr [][]string
+	for lineIndex := range fileArray{
+		line := fileArray[lineIndex]
+		if line != ""{
+			// Turn this into an array
+			charSlice := strings.Split(line, "")
+			arr = append(arr, charSlice)
+
+		}
+	}
+	return arr
+
+}
+// This function checks if you can access a specific element by checking the ones around it.
+func canAccess(arr [][]string, row int, column int) bool{
+	rowNum := len(arr)
+	columnNum := len(arr[0])
+	// fmt.Println("Max number of rows is", rowNum, "Max columns is", columnNum)
+	// if (row > 0 || column > 0) && (row < rowNum - 1 || column < columnNum - 1){
+		// This means not the borders.
+	element := arr[row][column]
+	count := 0
+	if element == "@"{
+		for i := range 3 {
+			for j := range 3{
+				currRow := (row - 1) + i
+				currColumn := (column - 1) + j
+				// fmt.Println(currRow, currColumn)
+				if !(currRow == row && currColumn == column){
+					// if (currRow >= 0 || currColumn >= 0) && (currRow <= rowNum - 1 || currColumn <= columnNum - 1){
+					// }
+					if (currRow >= 0 && currRow <= rowNum - 1) && (currColumn >= 0 && currColumn <= columnNum - 1){
+						// fmt.Println(currRow, currColumn)
+						if arr[currRow][currColumn] == "@"{
+							count += 1
+						}
+
+					}
+				}
+			}
+		}
+		if count < 4{
+			// fmt.Println(count)
+			return true
+		} else{
+			return false
+		}
+	}
+	return false
+}
+
+func forkLiftAccess(filename string) int{
+	arr := arrayInit(filename)
+	rowNum := len(arr)
+	columnNum := len(arr[0])
+	for line := range arr{
+		fmt.Println(arr[line])
+	}
+	fmt.Println("----")
+	count := 0
+	for i := range rowNum{
+		for j := range columnNum{
+			if arr[i][j] == "@"{
+				access := canAccess(arr, i, j)
+				if access{
+					// arr[i][j] = "x"
+					count ++
+				}
+			}
+			// fmt.Printf("Can access arr[%d][%d] = %t\n", i, j, access)
+		}
+	}
+	// for line := range arr{
+	// 	fmt.Println(arr[line])
+	// }
+	fmt.Println("Number of accessable rolls =", count)
+	return count
+
+}
+
+func main() {
+	fmt.Printf("Hello World.")
+}
---
+++ day-4/main_test.go
@@ -0,0 +1,15 @@
+package day4
+
+import (
+	"testing"
+)
+
+func TestInput1(t *testing.T){
+	forkLiftAccess("input.txt")
+
+}
+
+func TestInput2(t *testing.T){
+	forkLiftAccess("input2.txt")
+
+}
